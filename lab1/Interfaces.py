0ea980e6acc929015b3dfac3094de5e7f99a427f
IvKorolev <mrvanivan@mail.ru>
2025-11-20 13:25:21 +0300

    minor fixes

--- lab1/Interfaces.py
+++ lab1/Interfaces.py
@@ -1,22 +1,22 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
-from typing import Protocol, runtime_checkable, List
+from typing import List
 
-@runtime_checkable
-class KeySchedule(Protocol):
+class KeySchedule(ABC):
+    @abstractmethod
     def round_keys(self, master_key: bytes) -> List[bytes]:
         ...
 
 
-@runtime_checkable
-class RoundFunction(Protocol):
+class RoundFunction(ABC):
+    @abstractmethod
     def F(self, right_half: bytes, round_key: bytes) -> bytes:
         ...
 
 
 class SymmetricBlockCipher(ABC):
 
-    block_size: int  # байты
+    block_size: int
 
     @abstractmethod
     def configure(self, key: bytes) -> None:
